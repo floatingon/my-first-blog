5abf79e1c9b1db2561cb42ba4c3ff4483fb66b13
Lyon <lyon.r.anderson@gmail.com>
2020-05-02 22:34:44 -0600

    Disable debug

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '3m1(5qb0g8-qab$zpcjx=p9@4jb!$xi=h(^-#)9hk_(qodp^k*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
